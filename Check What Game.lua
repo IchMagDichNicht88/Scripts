42a27e9c8af406f673bb64e53141b7a5b1e96938
IchMagDichNicht88 <discordistgeil348@gmail.com>
2025-07-04 15:43:08 +0200

    Update Check What Game.lua

--- Check What Game.lua
+++ Check What Game.lua
@@ -3,42 +3,42 @@ local placeId = game.PlaceId
 --/ Crash Out Simulator
 if placeId == 15839834917 then
     print("Crash Out Simulator")
-loadstring(game:HttpGet("https://pastefy.app/6j0CmXgN/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Crash%20Out%20Simulator.lua", true))()
 
 --/ Cash Incremental
 elseif placeId == 129159449618378 then
     print("Cash Incremental")
-loadstring(game:HttpGet("https://pastefy.app/cxkvygky/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Cash%20Incremental.lua", true))()
 
 --/ Untitled Drill Game
 elseif placeId == 87700573492940 then
     print("Untitled Drill Game")
-loadstring(game:HttpGet("https://pastefy.app/dL1HWV28/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/untitled%20drill%20game.lua", true))()
 
 --/ Factory RNG
 elseif placeId == 76381016848158 then
     print("daddyFactory RNG")
-loadstring(game:HttpGet("https://pastefy.app/gv7Iw26M/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Factory%20RNG.lua", true))()
   
   --/ Egg Incremental
 elseif placeId == 137034315542002 then
     print("Egg Incremental")
-loadstring(game:HttpGet("https://pastefy.app/ismiGrdU/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Egg%20Incremental.lua", true))()
 
   --/ Egg Factory RNG
 elseif placeId == 97728924322450 then
     print("Egg Factory RNG")
-loadstring(game:HttpGet("https://pastefy.app/9svbNINq/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Egg%20Factory%20RNG.lua", true))()
 
   --/ Grow A Garden
 elseif placeId == 126884695634066 then
     print("Grow A Garden")
-loadstring(game:HttpGet("https://pastefy.app/MRXVZvLH/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Grow%20A%20Garden.lua", true))()
   
     --/ Ore Smelting Tycoon
 elseif placeId == 10057350313 then
     print("Ore Smelting Tycoon")
-loadstring(game:HttpGet("https://pastefy.app/VIv9glyi/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IchMagDichNicht88/Scripts/refs/heads/main/Ore%20Smelting%20Tycoon.lua", true))()
 
 
 else
