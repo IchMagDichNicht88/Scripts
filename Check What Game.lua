39df31eff60cbf8d665f3c884213faffe8f04306
IchMagDichNicht88 <discordistgeil348@gmail.com>
2025-07-04 15:38:37 +0200

    Create Check What Game.lua

---
+++ Check What Game.lua
@@ -0,0 +1,46 @@
+local placeId = game.PlaceId
+
+--/ Crash Out Simulator
+if placeId == 15839834917 then
+    print("Crash Out Simulator")
+loadstring(game:HttpGet("https://pastefy.app/6j0CmXgN/raw", true))()
+
+--/ Cash Incremental
+elseif placeId == 129159449618378 then
+    print("Cash Incremental")
+loadstring(game:HttpGet("https://pastefy.app/cxkvygky/raw", true))()
+
+--/ Untitled Drill Game
+elseif placeId == 87700573492940 then
+    print("Untitled Drill Game")
+loadstring(game:HttpGet("https://pastefy.app/dL1HWV28/raw", true))()
+
+--/ Factory RNG
+elseif placeId == 76381016848158 then
+    print("daddyFactory RNG")
+loadstring(game:HttpGet("https://pastefy.app/gv7Iw26M/raw", true))()
+  
+  --/ Egg Incremental
+elseif placeId == 137034315542002 then
+    print("Egg Incremental")
+loadstring(game:HttpGet("https://pastefy.app/ismiGrdU/raw", true))()
+
+  --/ Egg Factory RNG
+elseif placeId == 97728924322450 then
+    print("Egg Factory RNG")
+loadstring(game:HttpGet("https://pastefy.app/9svbNINq/raw", true))()
+
+  --/ Grow A Garden
+elseif placeId == 126884695634066 then
+    print("Grow A Garden")
+loadstring(game:HttpGet("https://pastefy.app/MRXVZvLH/raw", true))()
+  
+    --/ Ore Smelting Tycoon
+elseif placeId == 10057350313 then
+    print("Ore Smelting Tycoon")
+loadstring(game:HttpGet("https://pastefy.app/VIv9glyi/raw", true))()
+
+
+else
+    print("No Script Found For This Games")
+end
